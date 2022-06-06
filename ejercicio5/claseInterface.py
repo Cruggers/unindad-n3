37bdae35929b17c33092bc8fcf09267cfa8ac13e
Emiliano Quiroga <31597817+Cruggers@users.noreply.github.com>
2022-06-05 23:32:34 -0300

    Add files via upload

---
+++ ejercicio5/claseInterface.py
@@ -0,0 +1,12 @@
+from zope.interface import Interface
+
+class claseInterface ( Interface ):
+
+    def insertarElemento (elemento, pos):
+        pass
+
+    def agregaElemento (elemento):
+        pass
+
+    def mostrarElemento (pos):
+        pass
